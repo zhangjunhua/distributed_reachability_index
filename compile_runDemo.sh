d0a4da88b536cb101463cc32e7c0270299293c09
junhua zhnag <2414890918@qq.com>
2021-11-04 19:45:44 +1100

    add example graph

--- compile_runDemo.sh
+++ compile_runDemo.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 
+#set your gcc path
+export PATH=/opt/rh/devtoolset-9/root/usr/bin:$PATH
+
+#set env variables, and add corresponding env variables to .bashrc in each node
+export PATH=$PATH:/usr/lib64/mpich/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64/mpich/lib
+
+#compile code
 make clean
 make
 
+#create index
+mpiexec -n 2 ./drl -if example/graph -O 4
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 source code for distributed reachability labeling
-# prerequisite
+# prerequisites
  - mpich 3.0.4
  - gcc 4.8.5 or higher
 # run the demo
